60323397a64e079724ca19e3d3af0905ef5261bc
victordouradoribeiro <vdouradorb@gmail.com>
2025-08-09 12:45:08 -0300

    Estruturar arquitetura do projeto

---
+++ includes/header.php
@@ -0,0 +1,20 @@
+<!-- header.php -->
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+    <!-- Fonte -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap">
+
+    <!-- CSS personalizado -->
+    <link rel="stylesheet" href="css/styles.css">
+
+    <title>Vota Comunidade</title>
+</head>
+<body>
